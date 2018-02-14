0c62fa9872ed3b67244da42cd3b4ccf586dafdc4
mozukraken <mozukraken@gmail.com>
2018-02-14 23:28:26 +0900

    画像付きツイート

--- bot.php
+++ bot.php
@@ -13,8 +13,26 @@ define('ACCESS_TOKEN_SECRET', '');
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
-$content = $connection->get("account/verify_credentials");
+// アカウント情報の取得
+// $content = $connection->get("account/verify_credentials");
+// タイムラインから3件のツイートを取得
+// $content = $connection->get("statuses/home_timeline", ['count' => 3]);
+// 画像のアップロード
+$media = $connection->upload("media/upload", [
+  'media' => __DIR__ . '/test.jpg'
+]);
+// ツイート
+$res = $connection->post("statuses/update", [
+  'status' => 'これは画像付きテストツイートです',
+  // 画像のidを指定
+  'media_ids' => $media->media_id
+]);
 
-var_dump($content);
+if ($connection->getLastHttpCode() === 200) {
+  echo 'Success!' . PHP_EOL;
+} else {
+  echo 'Error!' . $res->errors[0]->message . PHP_EOL;
+}
+// var_dump($content);
 
 ?>
